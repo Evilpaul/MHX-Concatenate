10c3a4b807b5bcda6f3f8a1581f7fefd48523743
Evilpaul <yj589794@googlemail.com>
2011-09-10 03:28:56 +0100

    file dialogs, fancy

--- Form1.cs
+++ Form1.cs
@@ -55,5 +55,51 @@ namespace WindowsFormsApplication1
         {
 
         }
+
+        private void saveFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = "mhx";
+            saveDialog.AddExtension = true;
+            saveDialog.InitialDirectory = @"C:\Users\YourName\Documents\";
+            saveDialog.OverwritePrompt = true;
+            saveDialog.Title = "Bell Ringers";
+            saveDialog.ValidateNames = true;
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+            }
+            textBox2.Text = result.ToString(); // <-- For debugging use only.
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+            }
+            textBox3.Text = result.ToString(); // <-- For debugging use only.
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+            }
+            textBox4.Text = result.ToString(); // <-- For debugging use only.
+        }
     }
 }
